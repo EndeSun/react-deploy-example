22a401bc6eed7b13d4801924515d3a026b601ff8
ende <1195562121ende@gmail.com>
2023-08-04 15:18:44 +0200

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Mi primer tarea",
+        description: "descripción primer tarea"
+    },
+    {
+        id: 1,
+        title: "Mi segunda tarea",
+        description: "descripción segunda tarea"
+    },
+    {
+        id: 2,
+        title: "Mi tercera tarea",
+        description: "descripción tercera tarea"
+    }
+]
